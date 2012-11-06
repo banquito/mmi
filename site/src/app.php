3e208955957f1bd207c1c9129c4a78e4ac2b5724
Clodo <claudiobidau@gmail.com>
2012-11-05 22:03:55 -0300

    esqueleto silex (php framework)

---
+++ site/src/app.php
@@ -0,0 +1,28 @@
+<?php
+
+use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\UrlGeneratorServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+use Silex\Provider\HttpCacheServiceProvider;
+
+$app = new Application();
+
+$app->register(new UrlGeneratorServiceProvider());
+$app->register(new ValidatorServiceProvider());
+$app->register(new TwigServiceProvider(), array(
+    'twig.path'    => array(__DIR__.'/../templates'),
+    'twig.options' => array('cache' => __DIR__.'/../cache'),
+));
+$app->register(new HttpCacheServiceProvider(), array(
+    'http_cache.cache_dir' => __DIR__.'/../cache/',
+));
+
+
+$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+    // add custom globals, filters, tags, ...
+
+    return $twig;
+}));
+
+return $app;
---
+++ site/src/controllers.php
@@ -0,0 +1,21 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+$app->get('/', function () use ($app) {
+    $html = $app['twig']->render('home.html.twig');
+    return new Response($html, 200, array('Cache-Control' => 's-maxage=3600, public'));
+})->bind('homepage');
+
+$app->error(function (\Exception $e, $code) use ($app) {
+    if ($app['debug']) {
+        return;
+    }
+
+    $page = 404 == $code ? '404.html.twig' : '500.html.twig';
+
+    return new Response($app['twig']->render($page, array('code' => $code)), $code);
+});
