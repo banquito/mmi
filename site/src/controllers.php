6bdaca5286a23536b174e6ac4bdb891e79a4a4e4
Clodo <claudiobidau@gmail.com>
2012-11-08 00:10:23 -0300

    ajustes css y paginas estaticas

--- site/src/controllers.php
+++ site/src/controllers.php
@@ -10,6 +10,26 @@ $app->get('/', function () use ($app) {
     return new Response($html, 200, array('Cache-Control' => 's-maxage=3600, public'));
 })->bind('homepage');
 
+$app->get('/nosotros', function () use ($app) {
+    $html = $app['twig']->render('nosotros.html.twig');
+    return new Response($html, 200, array('Cache-Control' => 's-maxage=3600, public'));
+})->bind('nosotros');
+
+$app->get('/proyecto', function () use ($app) {
+    $html = $app['twig']->render('proyecto.html.twig');
+    return new Response($html, 200, array('Cache-Control' => 's-maxage=3600, public'));
+})->bind('proyecto');
+
+$app->get('/ayuda', function () use ($app) {
+    $html = $app['twig']->render('ayuda.html.twig');
+    return new Response($html, 200, array('Cache-Control' => 's-maxage=3600, public'));
+})->bind('ayuda');
+
+$app->get('/contacto', function () use ($app) {
+    $html = $app['twig']->render('contacto.html.twig');
+    return new Response($html, 200, array('Cache-Control' => 's-maxage=3600, public'));
+})->bind('contacto');
+
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
         return;
--- site/web/js/scripts.js
+++ site/web/js/scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     $("#slider").slider({
-        value:2010,
+        value:2006,
         min: 2006,
         max: 2010,
         step: 1,
         slide: function(event, ui) {
+            $( "#slider-current" ).html(ui.value);
         },
         change: function(event, ui) {
         }
