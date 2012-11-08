2f01be0e8207ae901258ed7709408d0f4e74dd23
Clodo <claudiobidau@gmail.com>
2012-11-07 21:30:39 -0300

    ajustes css maqueta

--- site/web/js/scripts.js
+++ site/web/js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  // Handler for .ready() called.
+    $("#slider").slider({
+        value:2010,
+        min: 2006,
+        max: 2010,
+        step: 1,
+        slide: function(event, ui) {
+        },
+        change: function(event, ui) {
+        }
+    });
 });
